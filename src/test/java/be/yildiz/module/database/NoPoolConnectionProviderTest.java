1f30e4f96acb06368b884731ca9d4d7d93fbc1fa
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-01-19 12:03:48 +0100

    [YIL-83] Sonar fixes.

--- src/test/java/be/yildiz/module/database/NoPoolConnectionProviderTest.java
+++ src/test/java/be/yildiz/module/database/NoPoolConnectionProviderTest.java
@@ -1,5 +1,6 @@
 package be.yildiz.module.database;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -27,6 +28,31 @@ public class NoPoolConnectionProviderTest {
         @Test(expected = IllegalArgumentException.class)
         public void withPropertiesNull() throws SQLException {
             new NoPoolConnectionProvider(DataBaseConnectionProvider.DBSystem.MYSQL, null);
+        }
+    }
+
+    public static class GetConnection {
+
+        @Test
+        public void happyFlow() throws SQLException {
+            DataBaseConnectionProvider c = new NoPoolConnectionProvider(DataBaseConnectionProvider.DBSystem.DERBY_IN_MEMORY, new TestingDatabaseInit.TestingDbProperties());
+            Assert.assertNotNull(c.getConnection());
+        }
+    }
+
+    public static class Close {
+
+        @Test
+        public void happyFlow() throws Exception {
+            DataBaseConnectionProvider c = new NoPoolConnectionProvider(DataBaseConnectionProvider.DBSystem.DERBY_IN_MEMORY, new TestingDatabaseInit.TestingDbProperties());
+            c.getConnection();
+            c.close();
+        }
+
+        @Test(expected = SQLException.class)
+        public void withNoConnection() throws Exception {
+            DataBaseConnectionProvider c = new NoPoolConnectionProvider(DataBaseConnectionProvider.DBSystem.DERBY_IN_MEMORY, new TestingDatabaseInit.TestingDbProperties());
+            c.close();
         }
     }
 }
